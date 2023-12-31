14dd4e4a7947baaca93497238da3672e5d8e1bf7
Vikas Kumar <vikasdhaka.nitk@gmail.com>
2023-12-31 23:35:46 +0530

    al : commit 50 : auth mid ware and other things

--- app/services/jobs-api/handlers/handlers.go
+++ app/services/jobs-api/handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/VikasKumar1187/publisher/app/services/jobs-api/handlers/v1/testgrp"
+	"github.com/VikasKumar1187/publisher/business/web/auth"
 	"github.com/VikasKumar1187/publisher/business/web/v1/mid"
 	"github.com/VikasKumar1187/publisher/foundation/web"
 	"go.uber.org/zap"
@@ -14,6 +15,7 @@ import (
 type APIMuxConfig struct {
 	Shutdown chan os.Signal
 	Log      *zap.SugaredLogger
+	Auth     *auth.Auth
 }
 
 // APIMux constructs a http.Handler with all application routes defined.
@@ -21,6 +23,7 @@ func APIMux(cfg APIMuxConfig) *web.App {
 	app := web.NewApp(cfg.Shutdown, mid.Logger(cfg.Log), mid.Errors(cfg.Log), mid.Metrics(), mid.Panics())
 
 	app.Handle(http.MethodGet, "/test", testgrp.Test)
+	app.Handle(http.MethodGet, "/test/auth", testgrp.Test, mid.Authenticate(cfg.Auth), mid.Authorize(cfg.Auth, auth.RuleAdminOnly))
 
 	return app
 }
--- app/services/jobs-api/main.go
+++ app/services/jobs-api/main.go
@@ -12,7 +12,9 @@ import (
 	"time"
 
 	"github.com/VikasKumar1187/publisher/app/services/jobs-api/handlers"
+	"github.com/VikasKumar1187/publisher/business/web/auth"
 	"github.com/VikasKumar1187/publisher/business/web/v1/debug"
+	"github.com/VikasKumar1187/publisher/foundation/keystore"
 	"github.com/VikasKumar1187/publisher/foundation/logger"
 	"github.com/ardanlabs/conf/v3"
 	"go.uber.org/zap"
@@ -54,6 +56,11 @@ func run(log *zap.SugaredLogger) error {
 			APIHost         string        `conf:"default:0.0.0.0:3000"`
 			DebugHost       string        `conf:"default:0.0.0.0:4000"`
 		}
+		Auth struct {
+			KeysFolder string `conf:"default:zarf/keys/"`
+			ActiveKID  string `conf:"default:54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"`
+			Issuer     string `conf:"default:service project"`
+		}
 	}{
 		Version: conf.Version{
 			Build: build,
@@ -83,6 +90,27 @@ func run(log *zap.SugaredLogger) error {
 	log.Infow("startup", "config", out)
 
 	// -------------------------------------------------------------------------
+	// Initialize authentication support
+
+	log.Infow("startup", "status", "initializing authentication support")
+
+	// Simple keystore versus using Vault.
+	ks, err := keystore.NewFS(os.DirFS(cfg.Auth.KeysFolder))
+	if err != nil {
+		return fmt.Errorf("reading keys: %w", err)
+	}
+
+	authCfg := auth.Config{
+		Log:       log,
+		KeyLookup: ks,
+	}
+
+	auth, err := auth.New(authCfg)
+	if err != nil {
+		return fmt.Errorf("constructing auth: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
 	// Start Debug Service
 
 	log.Infow("startup", "status", "debug v1 router started", "host", cfg.Web.DebugHost)
@@ -104,6 +132,7 @@ func run(log *zap.SugaredLogger) error {
 	apiMux := handlers.APIMux(handlers.APIMuxConfig{
 		Shutdown: shutdown,
 		Log:      log,
+		Auth:     auth,
 	})
 
 	api := http.Server{
